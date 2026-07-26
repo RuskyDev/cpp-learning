91df00c2302e9695925f71536243cba03bce975d
Ayaan Ali <iamayaanalee@gmail.com>
2026-07-26 20:23:19 +0500

    Add number guessing judge

---
+++ challenges/12_number_guessing_judge.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    int correctNumber = 50;
+    int input;
+
+    std::cout << "==========================\n";
+    std::cout << "Number Guessing Judge\n";
+    std::cout << "==========================\n\n";
+
+    std::cin >> input;
+    std::cout << "\n";
+    
+    std::cout << "==========================\n\n";
+
+    if (input == correctNumber) {
+        std::cout << "Correct!\n\n";
+    }
+    else if (input > 50) {
+        std::cout << "Too High.\n\n";
+    }
+    else {
+        std::cout << "Too Low.\n\n";
+    }
+    std::cout << "Thanks for playing Number Guessing Judge\n";
+
+    return 0;
+}
