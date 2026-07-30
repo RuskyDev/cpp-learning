98c551e9247980196d716c4a658b8a5b0310c2f3
Ayaan Ali <iamayaanalee@gmail.com>
2026-07-31 00:27:54 +0500

    Add grid challenge

---
+++ challenges/19_grid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Returns:
+//*****
+//*****
+//*****
+
+int main() {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 5; j++) {
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
