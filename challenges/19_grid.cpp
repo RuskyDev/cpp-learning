188f52057e29f9ce53d6c82afc8776690ac7db98
Ayaan Ali <iamayaanalee@gmail.com>
2026-07-31 00:32:48 +0500

    Add varaible to change rows and columns size

--- challenges/19_grid.cpp
+++ challenges/19_grid.cpp
@@ -6,8 +6,11 @@
 //*****
 
 int main() {
-    for (int i = 1; i <= 3; i++) {
-        for (int j = 1; j <= 5; j++) {
+    int rows = 3;
+    int columns = 5;
+    
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= columns; j++) {
             std::cout << "*";
         }
         std::cout << "\n";
