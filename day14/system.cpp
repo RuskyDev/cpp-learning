3a0a5e7f56fd0b54886625afe966f06091a410e3
Ayaan Ali <iamayaanalee@gmail.com>
2026-08-03 13:13:37 +0500

    Add day 14

---
+++ day14/system.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    // int input;
+
+    // std::cout << "jdkadjajdl;ajdaksjdakjsdk:\n";
+
+    // will execute a shell command eg,. to clear the screen we will run clear command, not recommended for real applictations.
+    // system("clear");
+
+    // std::cout << "After: \n";
+
+    while (true) {
+        std::cout << "HELLO World";
+        system("clear");
+    }
+    return 0;
+}
