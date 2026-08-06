4e9aee8d15d91d6b03251b698c133e57c55a44e6
Ayaan Ali <iamayaanalee@gmail.com>
2026-08-06 09:48:06 +0500

    Add challenge 21

--- challenges/21_odd_numbers.cpp
+++ challenges/21_odd_numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     for (int i = 1; i <= 99; i++) {
         // It will divide i with 2 and will check if there is a remainder remaining and if doesn't divide fully fit eg,. 5 % 2 equals 1 because 2 fits into 5 twice with 1 left over.
         if (i % 2 != 0) {
-            // Return each odd number.
+            // print  each odd number.
             std::cout << i << "\n";
         }
     }
